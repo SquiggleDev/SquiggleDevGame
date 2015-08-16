69a0a229df6bb2149eb03c3a00bd11d6dc32b93b
Sara McCloskey <sara_mccloskey@hotmail.com>
2015-08-16 23:10:01 +0100

    Update Ant build script
    Updated Ant build script to include functionality for separate
    development and release jar files, and for creating javadoc
    documentation.
    
    Also added comments for the Ant build script and the InputHandler class.

--- src/ie/squiggledev/engine/input/InputHandler.java
+++ src/ie/squiggledev/engine/input/InputHandler.java
@@ -5,6 +5,12 @@ import ie.squiggledev.game.Game;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * This class handles input from the user during execution of the main method
+ * of the {@link Game} class.
+ *
+ * @author SquiggleDev
+ */
 public class InputHandler implements KeyListener {
     public Key up = new Key();
     public Key down = new Key();
@@ -15,29 +21,29 @@ public class InputHandler implements KeyListener {
         game.addKeyListener(this);
     }
 
+    @Override
     public void keyPressed(KeyEvent e) {
         toggleKey(e.getKeyCode(), true);
     }
-    
+
+    @Override
     public void keyReleased(KeyEvent e) {
         toggleKey(e.getKeyCode(), false);
     }
-    
+
+    @Override
     public void keyTyped(KeyEvent e) {
-    
+
     }
-    
+
     public void toggleKey(int keyCode, boolean isPressed) {
         if (keyCode == KeyEvent.VK_W || keyCode == KeyEvent.VK_UP) {
             up.toggle(isPressed);
-        }
-        if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN) {
+        } else if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN) {
             down.toggle(isPressed);
-        }
-        if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT) {
+        } else if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT) {
             left.toggle(isPressed);
-        }
-        if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) {
+        } else if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) {
             right.toggle(isPressed);
         }
     }
@@ -45,15 +51,15 @@ public class InputHandler implements KeyListener {
     public class Key {
         private int numTimesPressed = 0;
         private boolean pressed = false;
-        
+
         public int getNumTimesPressed() {
             return numTimesPressed;
         }
-        
+
         public boolean isPressed() {
             return pressed;
         }
-        
+
         public void toggle(boolean isPressed) {
             this.pressed = isPressed;
             if (isPressed) {
